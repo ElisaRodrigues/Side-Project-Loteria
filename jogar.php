173f92ad8095aaa67b026341e3252be4590d48aa
ElisaRodrigues <elisabeth.estudos@gmail.com>
2017-05-25 13:43:06 -0300

    Add files via upload

--- jogar.php
+++ jogar.php
@@ -6,11 +6,23 @@ define('LOTOMANIA',3);
 define('LOTOFACIL',4);
 
 $nome = $_GET['nome'];
-$jogoSelecionado = $_GET['jogo-selecionado'];
+$jogoSelecionado = $_GET['jogoSelecionado'];
+
+function regrasDoJogo($jogoSelecionado){
+
+    $regras = [
+            'mega'      =>['min_dezenas'=>6, 'max_dezenas'=>15,'possibilidades'=>60],
+            'quina'     =>['min_dezenas'=>5, 'max_dezenas'=>15,'possibilidades'=>80],
+            'mania'     =>['min_dezenas'=>50, 'max_dezenas'=>50,'possibilidades'=>100],
+            'facil'     =>['min_dezenas'=>15, 'max_dezenas'=>18,'possibilidades'=>25]
+    ];
+    return $regras['mega'];
+}
 
 switch ($jogoSelecionado){
     case MEGASENA:
-        echo "carregar regras mega";
+        $regras = regrasDoJogo(MEGASENA);
+        print_r($regras);
         break;
 
     case QUINA:
